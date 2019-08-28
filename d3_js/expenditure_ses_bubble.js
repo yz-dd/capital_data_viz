814ca797926732b42e131e9912c5717baaa9abe8
yz-dd <tedz@live.cn>
2019-08-28 11:27:38 -0700

    Aggregates 5 year expenditures, adds grid lines

--- d3_js/expenditure_ses_bubble.js
+++ d3_js/expenditure_ses_bubble.js
@@ -1,29 +1,42 @@
 $('document').ready(() => {
     // set the dimensions and margins of the graph
-    const margin = { top: 30, right: 50, bottom: 30, left: 50 },
+    const margin = { top: 30, right: 50, bottom: 100, left: 80 },
         width = 500 - margin.left - margin.right,
         height = 1600 - margin.top - margin.bottom;
 
     // append the svg object to the body of the page
-    const svg = d3.select("#my_dataviz")
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform",
-            "translate(" + margin.left + "," + margin.top + ")");
-
-    const bubble_group = svg.append('g')
-        .attr('class', 'bubble_svg');
+    const svg = d3.select('#my_dataviz')
+        .append('svg')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+        .attr('transform',
+            'translate(' + margin.left + ',' + margin.top + ')');
 
     const colorScale = ['orange', 'lightblue'];
 
     const render = data => {
 
-        let filtered_data = data.filter( d => {
-            if(d.MINCODE != ' ' && !isNaN(d.SES_INDEX)) 
+        let filtered_data = data.filter(d => {
+            if (d.MINCODE != ' ' && !isNaN(d.SES_INDEX))
                 return d;
         });
+
+        //adds total expenditures for each school by matching mincode
+        let exp_sum_data = [];
+        filtered_data.forEach(record => {
+            let existing = exp_sum_data.filter((d, i) => {
+                return d.MINCODE == record.MINCODE
+            });
+            if (existing.length) {
+                let existingIndex = exp_sum_data.indexOf(existing[0])
+                exp_sum_data[existingIndex].TOTAL_ACTUAL = exp_sum_data[existingIndex].TOTAL_ACTUAL + record.TOTAL_ACTUAL;
+            } else {
+                exp_sum_data.push(record);
+            }
+        });
+
+        console.log(exp_sum_data);
 
         const title = 'Capital Funding: Expenditures vs. SES Index';
         const xAxisLabel = 'Capital Expenditures';
@@ -34,41 +47,47 @@ $('document').ready(() => {
             .range([0, width]);
 
         const yScale = d3.scaleLinear()
-            .domain(d3.extent(filtered_data, d=> {return d.SES_INDEX;}))
-            .range([height, 0])
-            .nice(12);
-    
+            .domain(d3.extent(exp_sum_data, d => { return d.SES_INDEX; }))
+            .range([height, 0]);
+
         console.log(yScale.domain());
 
         // Add a scale for bubble size
         const rScale = d3.scaleLinear()
-            .domain(d3.extent(filtered_data, d => {return d.TOTAL_ACTUAL}))
+            .domain(d3.extent(exp_sum_data, d => { return d.TOTAL_ACTUAL }))
             .range([5, 60]);
 
-        const xAxis = d3.axisBottom(xScale);
+        const xAxis = d3.axisBottom(xScale)
+            .tickSize(-height);
 
-        const yAxis = d3.axisLeft(yScale);
+        const yAxis = d3.axisLeft(yScale)
+            .tickSize(-width);
 
-        const xAxisG = svg.append("g")
-            .attr("transform", "translate(0," + height + ")")
+        const xAxisG = svg.append('g')
+            .attr('transform', 'translate(0,' + height + ')')
+            .attr('class', 'xAxisG')
             .call(xAxis);
 
-        const yAxisG = svg.append("g")
+        const yAxisG = svg.append('g')
+            .attr('class', 'yAxisG')
             .call(yAxis);
 
-        xAxisG.remove();
+        //grids
+        xAxisG.select('.domain').remove();
+        xAxisG.selectAll('.tick text').remove();
+        $('.xAxisG g:nth-child(6)').attr('id', 'center_line')
         yAxisG.select('.domain').remove();
 
         xAxisG.append('text')
             .attr('class', 'axis-label')
-            .attr('y', 75)
+            .attr('y', 60)
             .attr('x', width / 2)
             .attr('fill', 'black')
             .text(xAxisLabel);
 
         yAxisG.append('text')
             .attr('class', 'axis-label')
-            .attr('y', -93)
+            .attr('y', -60)
             .attr('x', -height / 2)
             .attr('fill', 'black')
             .attr('transform', `rotate(-90)`)
@@ -80,25 +99,31 @@ $('document').ready(() => {
             .attr('y', -10)
             .text(title);
 
-        const simulation = d3.forceSimulation(filtered_data)
+        const bubble_group = svg.append('g')
+            .attr('class', 'bubble_svg');
+
+        const simulation = d3.forceSimulation(exp_sum_data)
             .force('charge', d3.forceManyBody().strength(1))
             .force('x', d3.forceX().x(function (d) {
                 return xScale(50);
             }))
             .force('y', d3.forceY().y(d => {
-                return yScale(d.SES_INDEX);}
+                return yScale(d.SES_INDEX);
+            }
             ))
-            .force('collision', d3.forceCollide().radius(d => { return rScale(d.TOTAL_ACTUAL)}))
+            .force('collision', d3.forceCollide().radius(d => { return rScale(d.TOTAL_ACTUAL) }))
             .on('tick', ticked);
 
         function ticked() {
             let bubble = bubble_group.selectAll('circle')
-                .data(filtered_data);
+                .data(exp_sum_data);
 
             bubble.enter()
                 .append('circle')
-                .attr('data-ses-index', d => {return d.SES_INDEX;})
-                .attr('r', d => {return rScale(d.TOTAL_ACTUAL)})
+                .attr('data-ses-index', d => { return d.SES_INDEX; })
+                .attr('data-mincode', d => { return d.MINCODE; })
+                .attr('data-facility', d => { return d.FACILITY; })
+                .attr('r', d => { return rScale(d.TOTAL_ACTUAL) })
                 .style('fill', d => {
                     if (d.SES_INDEX < 0) {
                         return colorScale[0];
@@ -107,11 +132,11 @@ $('document').ready(() => {
                     }
                 })
                 .merge(bubble)
-                .attr('cx', function (d) { 
+                .attr('cx', function (d) {
                     return d.x;
                 })
                 .attr('cy', function (d) {
-                  
+
                     return d.y;
                 })
                 .attr('class', 'bubble')
@@ -125,8 +150,8 @@ $('document').ready(() => {
     //read data and render 
     //Read the data
     d3.queue()
-        .defer(d3.csv, "./raw_data/capital_data_with_mincode.csv")
-        .defer(d3.csv, "./raw_data/school_ses.csv")
+        .defer(d3.csv, './raw_data/capital_data_with_mincode.csv')
+        .defer(d3.csv, './raw_data/school_ses.csv')
         .await((error, cap_data, ses_data) => {
             if (error) {
                 console.error('Something went wrong: ' + error);
